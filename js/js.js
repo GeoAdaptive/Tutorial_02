e94af5a7811db7e857480cbc89289a815946ead1
geoadaptiveweb <wwu@geoadaptive.com>
2017-12-18 12:41:57 -0500

    1218_2

--- js/js.js
+++ js/js.js
@@ -31,45 +31,71 @@ L.marker([-25.262, -57.581]).addTo(map)
 var Paraguay_Department = "https://raw.githubusercontent.com/GeoAdaptive/Resources_library/master/Example_Paraguay/data/ADM_PRY_DEP.geojson?token=AgSQK2E68ldQRZyJTVEkUhY-YQb960hYks5aNCaMwA%3D%3D";
 
 //use this function to download and create mappable objects
-$(document).ready(function(){
-  $.ajax(Paraguay_Department).done(function(data){
-    var parsedData = JSON.parse(data);
-    var LayerMappedPolygon = L.geoJSON(parsedData,
-      {
-        style: {opacity:1,width:0.5,color:'#85C1E9'},
-        pointToLayer: function (feature, latlng) {
-        return new L.Polygon(latlng, {
-        });
-      },
-    }).bindPopup('I\'m in Paraguay!')
-    .addTo(map);
-  })
-});
+// $(document).ready(function(){
+//   $.ajax(Paraguay_Department).done(function(data){
+//     var parsedData = JSON.parse(data);
+//     var LayerMappedPolygon = L.geoJSON(parsedData,
+//       {
+//         style: {opacity:1,width:0.5,color:'#85C1E9'},
+//         pointToLayer: function (feature, latlng) {
+//         return new L.Polygon(latlng, {
+//         });
+//       },
+//     }).bindPopup('I\'m in Paraguay!')
+//     .addTo(map);
+//   })
+// });
 
 
 //commenting the code above.
 //4. Tutorial 02: Details of mapping Points, Lines and Polygons
 //4.1 Mapping Multiple Points
-var PointsUrl = "";
+var PointsUrl = "https://raw.githubusercontent.com/GeoAdaptive/Tutorial_02/master/data/INFR_middleschool_Paraguay.geojson";
 $(document).ready(function(){
   $.ajax(PointsUrl).done(function(data){
-
+    var parsedData = JSON.parse(data);
+    var MappedObjects = L.geoJSON(parsedData,
+      {
+        style: {opacity:1,radius:1,width:0.5,color:'#85C1E9'},
+        pointToLayer: function (feature, latlng) {
+        return new L.circleMarker(latlng, {
+        });
+      },
+    }).bindPopup('I\'m a point!')
+    .addTo(map);
   });
-};
-
+});
 
 //4.2 Mapping Lines
-var LinesUrl = "";
+var LinesUrl = "https://raw.githubusercontent.com/GeoAdaptive/Tutorial_02/master/data/primaryroads.geojson";
 $(document).ready(function(){
   $.ajax(LinesUrl).done(function(data){
-
+    var parsedData = JSON.parse(data);
+    var MappedObjects = L.geoJSON(parsedData,
+      {
+        style: {opacity:1,width:0.5,color:'#D35400'},
+        pointToLayer: function (feature, latlng) {
+        return new L.Polygon(latlng, {
+        });
+      },
+    }).bindPopup('I\'m a polyline!')
+    .addTo(map);
   });
-};
+});
 
 //4.3 Mapping Polygons
-var PolygonsUrl = "";
+var PolygonsUrl = "https://raw.githubusercontent.com/GeoAdaptive/Tutorial_02/master/data/ADM_PRY_DEP.geojson";
 $(document).ready(function(){
   $.ajax(PolygonsUrl).done(function(data){
-
+    var parsedData = JSON.parse(data);
+    var MappedObjects = L.geoJSON(parsedData,
+      {
+        style: {opacity:1,width:0.5,color:'#F9E79F'},
+        pointToLayer: function (feature, latlng) {
+        return new L.Polygon(latlng, {
+        });
+      },
+    }).bindPopup('I\'m a polygon!')
+    .addTo(map);
   });
-};
+});
